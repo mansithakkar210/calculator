00384859ad0571c88adb596aeeca6d07e2493c62
Mansi Thakkar <mansithakkar210@gmail.com>
2026-01-19 15:02:58 -0500

    Initial Commit

---
+++ calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  calculator
+//
+//  Created by Mansi Thakkar on 2026-01-19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
